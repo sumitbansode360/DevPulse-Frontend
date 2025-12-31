495b16f41dcf422f0cb342bfd9ef76eee5446bbe
sumitbansode360 <sumitbansode360@gmail.com>
2025-12-31 22:24:52 +0530

    login url updated

--- app/page.tsx
+++ app/page.tsx
@@ -80,7 +80,7 @@ export default function SignupPage() {
     setErrors({});
     try {
       const res = await axios.post(
-        "http://127.0.0.1:8000/api/token/",
+        "https://devpulsebackend.pythonanywhere.com/api/token/",
         formData
       );
       const data = await res.data;
