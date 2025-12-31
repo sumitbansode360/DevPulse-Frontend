38a4536d7255daa7fb73d141b618b51457888c5f
sumitbansode360 <sumitbansode360@gmail.com>
2025-12-31 22:15:15 +0530

    backend url update

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // Use an environment variable for the base URL, with a fallback for development.
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api',
+  baseURL: 'https://devpulsebackend.pythonanywhere.com/api',
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
